34b8f9879c24ec67f39dc5e4f02de847e0944ba7
Amilson Junior <amilson_junior@outlook.com>
2016-04-25 14:55:32 -0300

    Update barbeiro_dorminhoco.c

--- barbeiro_dorminhoco.c
+++ barbeiro_dorminhoco.c
@@ -4,81 +4,87 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#define CLIENTES (10)
-#define CADEIRAS (5)
+/*
+    Definição do problema:
+    - Se não há clientes, o barbeiro adormece;
+    - Se a cadeira do barbeiro estiver livre, um cliente pode ser atendido imediatamente;
+    - O cliente espera pelo barbeiro se houver uma cadeira de espera vazia;
+    - Se não tiver onde sentar, o cliente vai embora.
+*/
 
-sem_t sem_cadeiras; //semaforo para cadeira ocupada
-sem_t sem_barbeiro; //ocupado ou nao
-sem_t sem_cadeira_barb; //semaforo para a cadeira do barbeiro
-sem_t sem_clientes; //Existe clientes ou nao
+#define N_CLIENTES (10)
+#define N_CADEIRAS (5)
 
-void* barbeiroFunction(void *arg);
-void* clienteFunction(void *arg);
+sem_t semf_cadeiras; //semaforo para as cadeiras de espera
+sem_t semf_cadeira_barbeiro; //semaforo para a cadeira do barbeiro
+sem_t semf_cliente; //semaforo para se existe ou nao cliente
+sem_t semf_barbeiro; //semaforo para o barbeiro (dormindo ou ocupado);
+
+//Procedimento que sera executado pela thread 'barbeiro'
+void *barbeiro(void *arg)
+{
+    while (1)
+    {
+        sem_wait(&semf_cliente);
+        sleep(2); //2 segundos para fazer o servico
+        printf("O barbeiro cortou o cabelo do cliente.\n");
+        sem_post(&semf_barbeiro); //Libera o barbeiro.
+    }
+}
+
+// ....... sera executado pela thread 'clientes[i]'
+void *cliente(void *arg)
+{
+    int id = *(int*)arg;
+    sleep(id % 3);
+
+    if(sem_trywait(&semf_cadeiras) == 0)
+    {
+        printf("O cliente %d entrou na barbearia.\n", id);
+        sem_wait(&semf_cadeira_barbeiro);
+        sem_post(&semf_cliente); //Acorda o barbeiro
+        sem_post(&semf_cadeiras); //libera uma cadeira de espera
+        printf("O cliente %d esta cortando o cabelo...\n", id);
+        sem_wait(&semf_barbeiro); //Espera ate que o barbeiro esteja disponivel
+        sem_post(&semf_cadeira_barbeiro); //Libera a cadeira o barbeiro
+        printf("Cliente %d saiu satisfeito.\n\n", id);
+    }
+    else
+    {
+        printf("O cliente %d nao entrou na barbearia porque estava lotada.\n", id);
+    }
+
+    pthread_exit(NULL);
+}
 
 int main()
 {
-  sem_init(&sem_cadeiras, 0, CADEIRAS);
-  sem_init(&sem_barbeiro, 0, 0);
-  sem_init(&sem_cadeira_barb, 0, 1);
-  sem_init(&sem_clientes, 0, 0);
+    sem_init(&semf_cadeiras, 0, N_CADEIRAS);
+    sem_init(&semf_cadeira_barbeiro, 0, 1);
+    sem_init(&semf_cliente, 0, 0);
+    sem_init(&semf_barbeiro, 0, 0);
 
-  pthread_t clientes[CLIENTES];
-  pthread_t barbeiro;
+    pthread_t tbarbeiro;
+    pthread_t tclientes[N_CLIENTES];
 
-  int i, ids[CLIENTES];
-  for(i = 0; i < CLIENTES; i++)
-  {
-    ids[i] = i + 1;
-    pthread_create(&clientes[i], NULL, clienteFunction, (void *)&ids[i]);
-  }
+    int i;
+    int ids[N_CLIENTES];
 
-  pthread_create(&barbeiro, NULL, barbeiroFunction, NULL);
+    //Cria as threads dos clientes
+    for(i = 0; i < N_CLIENTES; i++)
+    {
+        ids[i] = i + 1;
+        pthread_create(&tclientes[i], NULL, cliente, (void*)&ids[i]);
+    }
 
-  //Inicializacao das threads;
-  for(i = 0; i < CLIENTES; i++)
-  {
-    pthread_join(clientes[i], NULL);
-  }
+    //cria a thread do barbeiro
+    pthread_create(&tbarbeiro, NULL, barbeiro, NULL);
 
-  return 0;
-}
+    //Coloca os clientes em concorrencia.
+    for(i = 0; i < N_CLIENTES; i++)
+    {
+        pthread_join(tclientes[i], NULL);
+    }
 
-void* barbeiroFunction(void *arg)
-{
-  while(1)
-  {
-    sem_wait(&sem_clientes); //o barbeiro dorme ou acorda.
-    printf("O barbeiro cortou o cabelo de um cliente.\n");
-    sem_post(&sem_barbeiro);
-  }
-
-  return NULL;
-}
-
-void* clienteFunction(void *arg)
-{
-  int id = *(int*)arg;
-  sleep(id % 3);
-
-  //sem_trywait
-  if(sem_trywait(&sem_cadeiras) == 0) //entrou na barbearia
-  {
-    printf("Cliente %d entrou na barbearia.\n", id);
-    
-    sem_wait(&sem_cadeira_barb); //espera a cadeira do barbeiro ser liberada.
-      printf("Cliente %d esta cortando o cabelo.\n", id);
-      sem_post(&sem_clientes); //cliente sai da cadeira de espera.
-      sem_post(&sem_cadeiras); //libera uma cadeira de espera.
-      sem_wait(&sem_barbeiro); //cliente esta cortando o cabelo
-    sem_post(&sem_cadeira_barb); // libera a cadeira do barbeiro.
-    
-    printf("O cliente %d deixou a barbearia.\n\n", id);
-  }
-  else //nao entrou na barbearia
-  {
-    printf("Cliente %d nao entrou na barbearia...\n", id);
-    return NULL;
-  }
-
-  pthread_exit(NULL);
+    return 0;
 }
